9ccf05a6eeed3024eba4a7cee2169d4d3b9ed2c7
Valentin <93677277+Vali644@users.noreply.github.com>
2024-01-28 16:51:41 +0200

    Add calculator object and basic math functions

---
+++ static/script.js
@@ -0,0 +1,9 @@
+const calculator = {
+    add: (a, b) => a + b,
+    substract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b,
+    firstNumber: undefined,
+    operand: undefined,
+    secondNumber: undefined,
+}
