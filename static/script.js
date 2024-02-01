43b7e0a9a7df525635cba4277f657732785cab1a
Valentin <93677277+Vali644@users.noreply.github.com>
2024-02-01 19:56:44 +0200

    Add ability to delete numbers

--- static/script.js
+++ static/script.js
@@ -3,9 +3,9 @@ const calculator = {
     substract: (a, b) => a - b,
     multiply: (a, b) => a * b,
     divide: (a, b) => a / b,
-    firstNumber: undefined,
-    operand: undefined,
-    secondNumber: undefined,
+    firstNumber: '0',
+    operand: '+',
+    secondNumber: '0',
     isComma: false,
     toInput: 1,
     operate (a, b, operator) {
@@ -31,33 +31,43 @@ const calculator = {
             document.getElementById("operation").innerText = operand;
         }
     },
-    input (number) {
+    input (number, remove) {
         if (this.toInput === 1) {
-            if (this.firstNumber === undefined) {
-                this.firstNumber = "";
-            }
-            if (this.firstNumber[0] === "0") {
-                if (number !== "0") {
-                    this.firstNumber = number;
+            if (remove) {
+                if (this.firstNumber.length === 1 || this.firstNumber.length === 0) {
+                    this.firstNumber = '0';
+                } else {
+                    this.firstNumber = this.firstNumber.slice(0, -1);
                 }
             } else {
-                this.firstNumber = this.firstNumber + number;
+                if (this.firstNumber[0] === "0") {
+                    if (number !== "0") {
+                        this.firstNumber = number;
+                    }
+                } else {
+                    this.firstNumber = this.firstNumber + number;
+                }
             }
-
-
         } else {
-            if (this.secondNumber === undefined) {
-                this.secondNumber = "";
-            }
-            if (this.secondNumber[0] === "0") {
-                if (number !== "0") {
+            if (remove) {
+                if (this.secondNumber.length === 1 || this.secondNumber.length === 0) {
+                    this.secondNumber = '0';
+                } else {
+                    this.secondNumber = this.secondNumber.slice(0, -1);
+                }
+            } else {
+                if (this.secondNumber[0] === "0") {
+                    if (number !== "0") {
                     this.secondNumber = number;
                 }
-            } else {
-                this.secondNumber = this.secondNumber + number;
+                } else {
+                    this.secondNumber = this.secondNumber + number;
+                }
             }
         }
         this.display(this.secondNumber, this.firstNumber, this.operand);
     },
 
 }
+
+// REMINDER: Implement the ability to correctly use the comma(dot), and to correctly remove it
